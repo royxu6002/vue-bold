f532c8510882b78dc89c8e9ff15f56aafa4e74bf
vagrant <vagrant@homestead>
2020-03-29 09:55:50 +0000

    try api

---
+++ src/api/bill.js
@@ -0,0 +1,9 @@
+const BILLFROM =  {
+	name: "Comlibra Electronic CO., Ltd",
+	address: "Room 423, Baolong Sqaure, Xiaoshan, Hangzhou, China",
+	tel: "+86 18258194466",
+	email: "export@comlibra.com"
+};
+
+
+export default BILLFROM;
