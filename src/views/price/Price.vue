c8a0cb7b4c31a1cf5b8adf0a0863874f657807c1
vagrant <vagrant@homestead>
2020-05-09 16:05:43 +0000

    fix cpt_no_tax, exw_no_tax

--- src/views/price/Price.vue
+++ src/views/price/Price.vue
@@ -885,7 +885,7 @@ export default {
     FOBCOST() {
       // 1 if productInfo.priceTerm == 'exw_no_tax';
       // 直接算出来拿退税和不拿退税的价格;
-      if(this.productInfo.priceTerm == 'exw_no_tax' && this.productInfo.totalSpace>0) {
+      if(this.productInfo.priceTerm == 'exw_no_tax' && this.productInfo.totalSpace>0 && this.productInfo.factoryTaxRate>0) {
         //拿退税, 
         let purchaseCost = this.productInfo.price*(1+this.productInfo.factoryTaxRate/100)*(100+VAT.COMMODITY-this.productInfo.refundRate)/(100+VAT.COMMODITY) + Number(this.qcModuleCost) + Number(this.shipmentModuleCost) + Number(this.cfsModuleCost) + Number(this.bankModuleCost) + Number(this.productModuleCost);
 
@@ -924,7 +924,7 @@ export default {
         return '$'+sellingCost + ' 总支出:¥'+purchaseCost+'拿退税<--|';
       }
       // 3 if productInfo.priceTerm = 'cpt_no_tax';
-      if (this.productInfo.priceTerm == 'cpt_no_tax' && this.productInfo.totalSpace>0) {
+      if (this.productInfo.priceTerm == 'cpt_no_tax' && this.productInfo.totalSpace>0 && this.productInfo.factoryTaxRate>0) {
         let purchaseCost = this.productInfo.price*(1+this.productInfo.factoryTaxRate/100)*(100+VAT.COMMODITY-this.productInfo.refundRate)/(100+VAT.COMMODITY) + Number(this.qcModuleCost) + Number(this.cfsModuleCost) + Number(this.bankModuleCost) + Number(this.productModuleCost);
 
         let CURRENCY = this.priceSystem.currencyRate;
